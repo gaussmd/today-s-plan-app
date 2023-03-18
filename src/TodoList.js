94e16a25ea982e15e71714a91f1357e1be0dba62
Mohammad Gaus <mohammadgaus2001@gmail.com>
2023-03-18 19:23:11 +0000

    working on funtion

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,10 +2,26 @@ import React, { useState } from 'react'
 
 function TodoList() {
   const [activity,setActivity] = useState('')
-
+  const [listData,setListData] = useState([]);
   function addActivity () {
-
-  }
+  //  setListData([...listData,activity])
+  //  console.log(listData)
+    setListData((listData)=> { 
+    const updatedList = [...listData,activity]
+    // console.log(updatedList)
+    setActivity('');
+    return updatedList
+    })
+}
+function removeTodos (i) {
+  const updataedListData = listData.filter((elem,id)=>{
+    return i!==id
+  })
+  setListData(updataedListData);
+}
+function removeAll () {
+  setListData([]);
+}
   return (
   <>
   <div className='content'>
@@ -15,7 +31,22 @@ function TodoList() {
     onChange={(e)=>setActivity(e.target.value)}
     />
     <button onClick={addActivity}>Add</button>
+    <h2>What To do !</h2>
+    {listData!==[] && listData.map((data,i)=>{
+      return(
+        <div key={i}>
+        <p className='list-item'> {data}</p>
+        <button className='btn' onClick={()=>removeTodos(i)}>Remove</button>
+        </div>
+      )
+    })}
+
+<div className='new-btn'>
+  {listData.length>=1 &&  <button onClick={removeAll}>Remove All</button>}
+   
   </div>
+  </div>
+
   </>
   )
 }
